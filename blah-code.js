91ce024323d9f2d21a98dc6833cb30c4406acd74
Michael Kühnel <m.kuehnel@micromata.de>
2016-01-10 13:50:34 +0100

    Adjust whitespace

--- blah-code.js
+++ blah-code.js
@@ -1,37 +1,37 @@
-(function (root, factory) {
+(function(root, factory) {
 
-  'use strict';
+	'use strict';
 
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], function () {
-      return (root.returnExportsGlobal = factory());
-    });
-  } else if (typeof exports === 'object') {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like enviroments that support module.exports,
-    // like Node.
-    module.exports = factory();
-  } else {
-    // Browser globals
-    root.returnExportsGlobal = factory();
-  }
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], function() {
+			return (root.returnExportsGlobal = factory());
+		});
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like enviroments that support module.exports,
+		// like Node.
+		module.exports = factory();
+	} else {
+		// Browser globals
+		root.returnExportsGlobal = factory();
+	}
 
-}(this, function () {
-// UMD Definition above, do not remove this line
+}(this, function() {
+	// UMD Definition above, do not remove this line
 
-// To get to know more about the Universal Module Definition
-// visit: https://github.com/umdjs/umd
+	// To get to know more about the Universal Module Definition
+	// visit: https://github.com/umdjs/umd
 
-  'use strict';
+	'use strict';
 
-  var blahCode = function blahCode() {
-    this.someProperty = 'value';
-  };
+	var blahCode = function blahCode() {
+		this.someProperty = 'value';
+	};
 
-  blahCode.prototype.someMethod = function (value) {
-    return value + this.someProperty;
-  };
+	blahCode.prototype.someMethod = function(value) {
+		return value + this.someProperty;
+	};
 
-  return blahCode;
+	return blahCode;
 }));
--- test/blah-codeSpec.js
+++ test/blah-codeSpec.js
@@ -2,17 +2,16 @@ var assert = require('assert');
 
 var blahCode = require('../blah-code');
 
-describe('blah-code', function () {
+describe('blah-code', function() {
 
-  var myinstance = new blahCode();
+	var myinstance = new blahCode();
 
-  it('should have someProperty', function () {
-    assert.equal(myinstance.someProperty, 'value');
-  });
+	it('should have someProperty', function() {
+		assert.equal(myinstance.someProperty, 'value');
+	});
 
-  it('should run someMethod', function () {
-    assert.equal(myinstance.someMethod('hello '), 'hello value');
-  });
+	it('should run someMethod', function() {
+		assert.equal(myinstance.someMethod('hello '), 'hello value');
+	});
 
 });
-
