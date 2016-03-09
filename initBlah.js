6c8ee9f2904b5d2d3ea30b8bf955d7efe430ff0d
Haroen Viaene <hello@haroen.me>
2016-03-09 21:06:31 +0100

    make instant
    changed to eventlistener on #input
    no more button
    changed some querySelector's to getElementById
    
    fixes #7

--- initBlah.js
+++ initBlah.js
@@ -1,22 +1,22 @@
-// ESLint config //
+
 /* eslint-env browser */
 /* global blahCode */
 
 document.addEventListener('DOMContentLoaded', function () {
 	'use strict';
-	var form = document.querySelectorAll('form')[0];
+	var form = document.getElementsByTagName('form')[0];
 	var radios = document.querySelectorAll('input[name="language"]');
 	for (var i = radios.length; i--;) {
 		radios[i].addEventListener('change', function (e) {
-			document.querySelectorAll('button')[0].innerHTML = document.querySelector('input[name="language"]:checked').value + ' …';
+			document.querySelector('button').innerHTML = document.querySelector('input[name="language"]:checked').value + ' …';
 		});
 	}
-	form.addEventListener('submit', function (e) {
+	document.getElementById('input').addEventListener('input', function (e) {
 		e.preventDefault();
-		if (document.querySelectorAll('#mode')[0].value === 'encode') {
-			document.querySelectorAll('#output')[0].value = blahCode.encode(document.querySelectorAll('#input')[0].value, document.querySelector('input[name="language"]:checked').value);
+		if (document.getElementById('mode').value === 'encode') {
+			document.getElementById('output').value = blahCode.encode(document.getElementById('input').value, document.querySelector('input[name="language"]:checked').value);
 		} else {
-			document.querySelectorAll('#output')[0].value = blahCode.decode(document.querySelectorAll('#input')[0].value, document.querySelector('input[name="language"]:checked').value);
+			document.getElementById('output').value = blahCode.decode(document.getElementById('input').value, document.querySelector('input[name="language"]:checked').value);
 		}
 	});
 });
